2245e9d82a92cad550d15ee6f7b18049f5d4c592
Jesús Pérez <jesusprubio@gmail.com>
2015-03-02 10:59:16 +0100

    Distribution files regenerated

--- dist/fullcalendar.js
+++ dist/fullcalendar.js
@@ -1,12 +1,15 @@
 /*!
  * FullCalendar v2.3.0
- * Docs & License: http://arshaw.com/fullcalendar/
- * (c) 2013 Adam Shaw
+ * Docs & License: http://fullcalendar.io/
+ * (c) 2015 Adam Shaw
  */
 
 (function(factory) {
 	if (typeof define === 'function' && define.amd) {
 		define([ 'jquery', 'moment' ], factory);
+	}
+	else if (typeof exports === 'object') { // Node/CommonJS
+		module.exports = factory(require('jquery'), require('moment'));
 	}
 	else {
 		factory(jQuery, moment);
@@ -10782,4 +10785,5 @@ fcViews.agendaDay = {
 };
 ;;
 
+return fc; // export for Node/CommonJS
 });
